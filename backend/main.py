c3ecd747093930575631d5341041bf5c089f9c8c
ReinforceZwei <shingleongchu@gmail.com>
2023-09-13 17:25:18 +0800

    So many routes

--- backend/main.py
+++ backend/main.py
@@ -19,18 +19,34 @@ app.include_router(api)
 @app.get("/")
 async def root():
     return ""
-    # return AnimeFull(
-    #     id=0,
-    #     user_id=0,
-    #     name="TEst",
-    #     create_time=datetime.now(),
-    #     watched_time=None,
-    #     downloaded=False,
-    #     watched=False,
-    #     rating=None,
-    #     url=None,
-    #     remark="Mark",
-    #     tmdb_id=None,
-    #     tags=[],
-    #     categories=[],
-    #     comment="Comment")
+
+# get /anime get details
+# post /anime create new anime
+# patch /anime update anime
+# delete /anime delete anime
+# get /anime/tag get tags
+# patch /anime/tag link tags to anime
+# delete /anime/tag delete linked tags
+# get /anime/category get category
+# patch /anime/category link category to anime
+# delete /anime/category delete linked category
+
+# get /tags get all tags
+# get /tag get tag details
+# get /tag/anime get anime with this tag
+# post /tag create tag
+# patch /tag update tag
+# delete /tag delete tag
+
+# get /categories get all categories
+# get /category get category details
+# get /category/anime get anime in this category
+# post /category create category
+# patch /category update category
+# delete /category delete category
+
+# get /user get user details
+# post /user create new user
+# post /user/login login user
+# get /user/settings get user settings
+# patch /user/settings update user settings
