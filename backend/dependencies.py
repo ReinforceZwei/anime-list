5d750c04bbe89a621b728fcfe4bd0f9963d31d8e
ReinforceZwei <shingleongchu@gmail.com>
2023-09-18 17:12:39 +0800

    Welcome to new hell of dependency

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2AuthorizationCodeBearer, HTTPBearer
 import fastapi.security
 from typing import Annotated, Any
@@ -31,7 +31,7 @@ def user_dao(db: Annotated[PooledMySQLConnection, Depends(db_session)]):
 def anime_dao(db: Annotated[PooledMySQLConnection, Depends(db_session)]):
     return AnimeDao(db)
 
-def get_current_user(token: Annotated[str, Depends(get_bearer)], user_dao: Annotated[UserDao, Depends(user_dao)]) -> User:
+def get_current_user(token: Annotated[str, Depends(get_bearer)], user_dao: Annotated[UserDao, Depends(user_dao)], request: Request) -> User:
     error_unauthorized = HTTPException(401, "Unauthorized")
     try:
         jwt_payload = decode_user_token_no_verify(token)
@@ -43,6 +43,7 @@ def get_current_user(token: Annotated[str, Depends(get_bearer)], user_dao: Annot
             # Verify JWT using user password
             decode_user_token(settings.secret_key, db_user.password, token)
             # No error then return user
+            request.state.user = db_user
             return db_user
     except HTTPException:
         raise
--- backend/router/anime.py
+++ backend/router/anime.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, Path
+from fastapi import APIRouter, Depends, Path, Request
 from typing import Annotated
-from model.anime import AnimeCreate
 
+from model.anime import AnimeCreate
+from model.user import User
 from dal.anime import AnimeDao
-from dependencies import anime_dao
+from dependencies import anime_dao, get_current_user
 
 # get /anime get details
 # post /anime create new anime
@@ -16,44 +17,44 @@ from dependencies import anime_dao
 # patch /anime/category link category to anime
 # delete /anime/category delete linked category
 
-router = APIRouter(prefix='/anime', tags=['anime'])
+router = APIRouter(prefix='/anime', tags=['anime'], dependencies=[Depends(get_current_user)])
 
 @router.post('/create')
-def create_anime(anime: AnimeCreate, anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def create_anime(anime: AnimeCreate, anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.get('/{id}')
-def get_anime(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def get_anime(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.patch('/{id}')
-def update_anime(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def update_anime(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.delete('/{id}')
-def delete_anime(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def delete_anime(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.get('/{id}/tag')
-def get_tag(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def get_tag(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.patch('/{id}/tag')
-def update_tag(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def update_tag(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.delete('/{id}/tag')
-def delete_tag(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def delete_tag(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.get('/{id}/category')
-def get_category(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def get_category(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.patch('/{id}/category')
-def update_category(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def update_category(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
 
 @router.delete('/{id}/category')
-def delete_category(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)]):
+def delete_category(id: Annotated[int, Path()], anime_dao: Annotated[AnimeDao, Depends(anime_dao)], user: Annotated[User, get_current_user]):
     pass
